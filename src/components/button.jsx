e8a2bb4f3f72dc242f56ef66a30c9674faccde07
Pushkar <git config --global user.email>
2025-03-12 11:30:19 +0530

    product card and button created

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,11 +1,7 @@
-import React from 'react';
+// write button card here
 
 const ViewProductButton = () => {
-  return (
-    <button className="mt-2 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition">
-      View Product
-    </button>
-  );
+  return <button className="button">View Product</button>;
 };
 
 export default ViewProductButton;
